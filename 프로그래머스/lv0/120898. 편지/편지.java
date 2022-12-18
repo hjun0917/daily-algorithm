85e3e0755c793ff49663ebbf444e277a33c5ceba
Kim Hyeong jun <91590391+hjun0917@users.noreply.github.com>
2022-12-19 01:21:16 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int solution(String message) {
+//         List<String> words = new ArrayList<>();
+
+//         for (int i = 0; i < message.length(); i++) {
+//             words.add(message.substring(i,i+1));
+//         }
+        
+//         return words.size()*2;
+        
+        return message.length()*2;
+    }
+}
