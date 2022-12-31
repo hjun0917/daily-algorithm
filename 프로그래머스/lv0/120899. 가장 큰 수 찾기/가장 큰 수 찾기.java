4a5ebf62faad50eb0189d309bb4acfc6b704585c
Kim Hyeong jun <91590391+hjun0917@users.noreply.github.com>
2023-01-01 00:36:52 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = {};
+        int bigNum = 0;
+        int index = -1;
+        for(int i = 0; i < array.length ; i++) {
+            if (bigNum < array[i]) {
+                bigNum = array[i];
+                index = i;
+            }
+        }
+        
+        answer = new int[]{bigNum, index};
+        
+        return answer;
+    }
+}
