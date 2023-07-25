9caa3cd567eac6b8912ff5334ddd62e680f70412
Kim Hyeong jun <91590391+hjun0917@users.noreply.github.com>
2023-07-25 17:35:13 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,14 @@
+function solution(strings, n) {
+
+    for (let i = 0; i < strings.length; i++) {
+        strings[i] = strings[i][n] + strings[i];
+    }
+    
+    strings.sort();
+    
+    for (let i = 0; i < strings.length; i++) {
+        strings[i] = strings[i].slice(1, strings[i].length);
+    }
+
+    return strings;
+}
